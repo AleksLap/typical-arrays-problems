63562833a5bf78048ea9367171d7a1c77b547527
AleksLap <a.lapanovich@gmail.com>
2020-09-29 17:59:47 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(typeof(array) !== 'undefined' && array.length !== 0){
+    let result = array[0];
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] < result) {
+        result = array[i];
+      }
+    }
+    return result;
+  } else {
+    return 0;
+  }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(typeof(array) !== 'undefined' && array.length !== 0){
+    let result = array[0];
+    for(let i = 1; i < array.length; i++) {
+      if(array[i] > result) {
+        result = array[i];
+      }
+    }
+    return result;
+  } else {
+    return 0;
+  }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+
+  if(typeof(array) !== 'undefined' && array.length !== 0){
+    for(let i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+    return result / array.length;
+  } else {
+    return 0;
+  }  
 }
